d672fcb6fc60513c9346c1b374fe82e2c474d788
yacodev <cyaco33@gmail.com>
2024-04-10 17:00:24 -0500

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-//import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './routes/AppRouter';
 import './App.scss';
 
 function App() {
   return (
-    <>
-      <p className='read-the-docs'>home page</p>
-    </>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Page/Home/index.tsx
@@ -0,0 +1,13 @@
+import { UserCard } from '../../Components/UserCard';
+import { HeaderPage } from '../../Components/HeaderPage';
+import Button from '@mui/material/Button';
+
+export const HomePage = () => {
+  return (
+    <>
+      <HeaderPage />
+      <UserCard name='carlos' latitude='20.667' longitude='-103.333' id={1} />
+      <Button> text</Button>
+    </>
+  );
+};
